b19f4ad159e6aee2bdd939b0f34be5de29ff9f63
john-mlr <john.miller@icahn.mssm.edu>
2021-03-30 19:11:18 -0400

    module moving

--- simclr/modules/__init__.py
+++ simclr/modules/__init__.py
@@ -1,5 +1,4 @@
 from .datasets import SimCLRCIFAR10
-from .models import SimCLR, LinearClassifier
 from .transforms import SimCLRTransforms
 from .nt_xent import NT_Xent
 from .make_features import make_features
