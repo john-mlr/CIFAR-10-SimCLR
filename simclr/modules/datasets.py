970e2cb257043f598574755e0c140cae55bd2a62
John Miller <john.d.miller95@gmail.com>
2021-04-01 11:02:07 -0400

    added docstrings

--- simclr/modules/datasets.py
+++ simclr/modules/datasets.py
@@ -1,6 +1,17 @@
 from torchvision import datasets
 
 class SimCLRCIFAR10(datasets.CIFAR10):
+    """ CIFAR-10 Dataset for SimCLR. If pretraining is selected, the set will return
+        the same image twice with transformations applied. If it is not, the set 
+        will return a transformed image and its label.
+    
+        Init args:
+            download_path (str: path): path to store images locally on.
+            train (bool: default True): If true, will draw imagesfrom the training split.
+                                        False will draw from test.
+            transforms (nn.Module or Object): transforms to apply to the images
+            pretraining (bool: default True): Whether the dataset is for pretraining.
+    """
     def __init__(self, download_path, train=True, transforms=None, pretraining=True):
         super().__init__(root=download_path, train=train, download=True)
         
@@ -11,7 +22,10 @@ class SimCLRCIFAR10(datasets.CIFAR10):
     # parent.
         
     def __getitem__(self, idx):
-        # retrieve images and labels using inherited get item.
+        """ Returns a pair of images or an image and its label. Draws idx from
+            the instances referenced DataLoader.
+        """
+        # retrieve images and labels using inherited __getitem__.
         img, label = super().__getitem__(idx)
         
         # pretraining does not require labels, so does not return them.
